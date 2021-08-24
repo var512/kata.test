305b03497e56de0c7915364a9c7a2826fe7f82d4
var512 <var512dev@gmail.com>
2021-08-23 21:29:02 -0300

    StringCalculator: test 1 refactor

--- tests/Unit/StringCalculatorTest.php
+++ tests/Unit/StringCalculatorTest.php
@@ -10,9 +10,6 @@ class StringCalculatorTest extends TestCase
     /** @test */
     public function empty_string_should_equal_zero()
     {
-        $stringCalculator = new StringCalculator();
-        $sum = $stringCalculator->add('');
-
-        $this->assertEquals(0, $sum);
+        $this->assertEquals(0, (new StringCalculator())->add(''));
     }
 }
