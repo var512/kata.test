bc6f8946fbdfa216f4dd05e35c56a7e168f90cbe
var512 <var512dev@gmail.com>
2021-08-30 15:42:55 -0300

    StringCalculator: add return type declarations

--- tests/Unit/Kata/StringCalculator/StringCalculatorTest.php
+++ tests/Unit/Kata/StringCalculator/StringCalculatorTest.php
@@ -10,45 +10,45 @@ use Tests\TestCase;
 class StringCalculatorTest extends TestCase
 {
     /** @test */
-    public function empty_string_should_equal_zero()
+    public function empty_string_should_equal_zero(): void
     {
         $this->assertEquals(0, (new StringCalculator())->add(''));
     }
 
     /** @test */
-    public function single_number_should_return_its_own_value()
+    public function single_number_should_return_its_own_value(): void
     {
         $number = '1';
         $this->assertEquals($number, (new StringCalculator())->add($number));
     }
 
     /** @test */
-    public function two_numbers_should_return_their_sum()
+    public function two_numbers_should_return_their_sum(): void
     {
         $this->assertEquals(3, (new StringCalculator())->add('1,2'));
     }
 
     /** @test */
-    public function multiple_numbers_should_return_their_sum()
+    public function multiple_numbers_should_return_their_sum(): void
     {
         $this->assertEquals(6, (new StringCalculator())->add('1,2,3'));
     }
 
     /** @test */
-    public function can_handle_new_lines_as_delimiter()
+    public function can_handle_new_lines_as_delimiter(): void
     {
         $this->assertEquals(6, (new StringCalculator())->add('1\n2,3'));
         $this->assertEquals(6, (new StringCalculator())->add('1\n2\n3'));
     }
 
     /** @test */
-    public function can_handle_a_specific_delimiter()
+    public function can_handle_a_specific_delimiter(): void
     {
         $this->assertEquals(3, (new StringCalculator())->add('//;\n1;2'));
     }
 
     /** @test */
-    public function negative_number_is_not_allowed()
+    public function negative_number_is_not_allowed(): void
     {
         $this->expectException(NegativeNumbersAreNotAllowed::class);
         $this->expectExceptionMessage('negatives not allowed -1');
@@ -56,7 +56,7 @@ class StringCalculatorTest extends TestCase
     }
 
     /** @test */
-    public function negative_numbers_are_not_allowed()
+    public function negative_numbers_are_not_allowed(): void
     {
         $this->expectException(NegativeNumbersAreNotAllowed::class);
         $this->expectExceptionMessage('negatives not allowed -1 -2 -3');
@@ -64,7 +64,7 @@ class StringCalculatorTest extends TestCase
     }
 
     /** @test */
-    public function can_count_calls_to_add()
+    public function can_count_calls_to_add(): void
     {
         $stringCalculator = new StringCalculator();
         $stringCalculator->add('1,2,3');
@@ -74,33 +74,33 @@ class StringCalculatorTest extends TestCase
     }
 
     /** @test */
-    public function adding_should_trigger_add_occurred_event()
+    public function adding_should_trigger_add_occurred_event(): void
     {
         $this->expectsEvents(AddOccurred::class);
         (new StringCalculator())->add('1,2');
     }
 
     /** @test */
-    public function numbers_bigger_than_1000_are_ignored()
+    public function numbers_bigger_than_1000_are_ignored(): void
     {
         $this->assertEquals(1, (new StringCalculator())->add('1,1001'));
         $this->assertEquals(1001, (new StringCalculator())->add('1,1000,1001'));
     }
 
     /** @test */
-    public function delimiters_can_be_of_any_length()
+    public function delimiters_can_be_of_any_length(): void
     {
         $this->assertEquals(6, (new StringCalculator())->add('//[***]\n1***2***3'));
     }
 
     /** @test */
-    public function allow_multiple_delimiters()
+    public function allow_multiple_delimiters(): void
     {
         $this->assertEquals(6, (new StringCalculator())->add('//[*][%]\n1*2%3'));
     }
 
     /** @test */
-    public function allow_multiple_delimiters_with_any_length()
+    public function allow_multiple_delimiters_with_any_length(): void
     {
         $this->assertEquals(6, (new StringCalculator())->add('//[**][%%]\n1**2%%3'));
     }
