dc5ae6cddeb47100ec57c6ffa3de4fad90626199
var512 <var512dev@gmail.com>
2021-08-30 15:49:08 -0300

    StringCalculator: add setUp; refactor

--- tests/Unit/Kata/StringCalculator/StringCalculatorTest.php
+++ tests/Unit/Kata/StringCalculator/StringCalculatorTest.php
@@ -9,42 +9,51 @@ use Tests\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
+    private StringCalculator $stringCalculator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->stringCalculator = new StringCalculator();
+    }
+
     /** @test */
     public function empty_string_should_equal_zero(): void
     {
-        $this->assertEquals(0, (new StringCalculator())->add(''));
+        $this->assertEquals(0, $this->stringCalculator->add(''));
     }
 
     /** @test */
     public function single_number_should_return_its_own_value(): void
     {
         $number = '1';
-        $this->assertEquals($number, (new StringCalculator())->add($number));
+        $this->assertEquals($number, $this->stringCalculator->add($number));
     }
 
     /** @test */
     public function two_numbers_should_return_their_sum(): void
     {
-        $this->assertEquals(3, (new StringCalculator())->add('1,2'));
+        $this->assertEquals(3, $this->stringCalculator->add('1,2'));
     }
 
     /** @test */
     public function multiple_numbers_should_return_their_sum(): void
     {
-        $this->assertEquals(6, (new StringCalculator())->add('1,2,3'));
+        $this->assertEquals(6, $this->stringCalculator->add('1,2,3'));
     }
 
     /** @test */
     public function can_handle_new_lines_as_delimiter(): void
     {
-        $this->assertEquals(6, (new StringCalculator())->add('1\n2,3'));
-        $this->assertEquals(6, (new StringCalculator())->add('1\n2\n3'));
+        $this->assertEquals(6, $this->stringCalculator->add('1\n2,3'));
+        $this->assertEquals(6, $this->stringCalculator->add('1\n2\n3'));
     }
 
     /** @test */
     public function can_handle_a_specific_delimiter(): void
     {
-        $this->assertEquals(3, (new StringCalculator())->add('//;\n1;2'));
+        $this->assertEquals(3, $this->stringCalculator->add('//;\n1;2'));
     }
 
     /** @test */
@@ -52,7 +61,7 @@ class StringCalculatorTest extends TestCase
     {
         $this->expectException(NegativeNumbersAreNotAllowed::class);
         $this->expectExceptionMessage('negatives not allowed -1');
-        (new StringCalculator())->add('-1,2,3');
+        $this->stringCalculator->add('-1,2,3');
     }
 
     /** @test */
@@ -60,48 +69,47 @@ class StringCalculatorTest extends TestCase
     {
         $this->expectException(NegativeNumbersAreNotAllowed::class);
         $this->expectExceptionMessage('negatives not allowed -1 -2 -3');
-        (new StringCalculator())->add('-1,-2,-3');
+        $this->stringCalculator->add('-1,-2,-3');
     }
 
     /** @test */
     public function can_count_calls_to_add(): void
     {
-        $stringCalculator = new StringCalculator();
-        $stringCalculator->add('1,2,3');
-        $stringCalculator->add('1,2,3');
-        $stringCalculator->add('1,2,3');
-        $this->assertEquals(3, $stringCalculator->getCalledCount());
+        $this->stringCalculator->add('');
+        $this->stringCalculator->add('0');
+        $this->stringCalculator->add('1,2');
+        $this->assertEquals(3, $this->stringCalculator->getCalledCount());
     }
 
     /** @test */
     public function adding_should_trigger_add_occurred_event(): void
     {
         $this->expectsEvents(AddOccurred::class);
-        (new StringCalculator())->add('1,2');
+        $this->stringCalculator->add('1,2');
     }
 
     /** @test */
     public function numbers_bigger_than_1000_are_ignored(): void
     {
-        $this->assertEquals(1, (new StringCalculator())->add('1,1001'));
-        $this->assertEquals(1001, (new StringCalculator())->add('1,1000,1001'));
+        $this->assertEquals(1, $this->stringCalculator->add('1,1001'));
+        $this->assertEquals(1001, $this->stringCalculator->add('1,1000,1001'));
     }
 
     /** @test */
     public function delimiters_can_be_of_any_length(): void
     {
-        $this->assertEquals(6, (new StringCalculator())->add('//[***]\n1***2***3'));
+        $this->assertEquals(6, $this->stringCalculator->add('//[***]\n1***2***3'));
     }
 
     /** @test */
     public function allow_multiple_delimiters(): void
     {
-        $this->assertEquals(6, (new StringCalculator())->add('//[*][%]\n1*2%3'));
+        $this->assertEquals(6, $this->stringCalculator->add('//[*][%]\n1*2%3'));
     }
 
     /** @test */
     public function allow_multiple_delimiters_with_any_length(): void
     {
-        $this->assertEquals(6, (new StringCalculator())->add('//[**][%%]\n1**2%%3'));
+        $this->assertEquals(6, $this->stringCalculator->add('//[**][%%]\n1**2%%3'));
     }
 }
